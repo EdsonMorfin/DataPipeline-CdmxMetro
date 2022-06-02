970ac4d401c67dbb497fbf92392f302ed790f7b1
EdsonMorfin <edsonpaul2018@gmail.com>
2022-06-02 10:35:50 -0500

    Get requests refactor

--- api_metro.py
+++ api_metro.py
@@ -1,67 +1,83 @@
-from datetime import date, datetime
-from textwrap import indent
-from typing import Union
-from pydantic import BaseModel
 from fastapi import FastAPI
 import psycopg2
 from psycopg2 import Error
-from psycopg2 import sql
 from dotenv import load_dotenv
 import json
 import os
-
+from database_sender import open_connection,close_connection
 app = FastAPI()
 load_dotenv()
-
-def close_connection(connection):
-    if (connection):
-        connection.close()
-        print("PostgreSQL connection is closed")
-
-def open_connection():
-    try:
-        # Connect to an existing database
-        connection = psycopg2.connect(user=os.getenv("USER"),
-                                    password=os.getenv("PASSWORD"),
-                                    host=os.getenv("HOST"),
-                                    port=os.getenv("PORT"),
-                                    database=os.getenv("DATABASE"))
-
-        # Create a cursor to perform database operations
-        cursor = connection.cursor()
-        # Print PostgreSQL details
-        print("PostgreSQL server information")
-        print(connection.get_dsn_parameters(), "\n")
-        # Executing a SQL query
-        cursor.execute("SELECT version();")
-        # Fetch result
-        record = cursor.fetchone()
-        print("You are connected to - ", record, "\n")
-
-    except (Exception, Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-    return connection,cursor
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hola_Soy": "EdsonPaulMorfin"}
 
 
-@app.get("/test")
+@app.get("/units")
 def get_unidades():
-    connection,cursor = open_connection()
-    get_all_unidades = "SELECT * FROM metro_position LIMIT 5"
-    cursor.execute(get_all_unidades)
-    colnames = [desc[0] for desc in cursor.description]
-    #colnames = json.dumps(colnames) # replace single quote with double quote
-    unidades = cursor.fetchall()
-    cursor.close()
-    close_connection(connection)
-    return get_json_response(colnames,unidades)
+    query_request = """
+                        SELECT * FROM metro_position
+                        ORDER BY
+                            DATE(date_updated)=DATE(NOW()) DESC,
+                            DATE(date_updated)<DATE(NOW()) DESC,
+                            DATE(date_updated)>DATE(NOW()) ASC
+                        """
+    column_names,query_response = get_query_response(query_request)
+    return get_json_response(column_names,query_response)
+
+
+@app.get("/unit/{id}") # by id
+async def get_unidades(id):
+    query_request = """
+                        SELECT * FROM metro_position
+                        WHERE
+                            vehicle_id = 1117
+                        ORDER BY
+                            DATE(date_updated)=DATE(NOW()) DESC,
+                            DATE(date_updated)<DATE(NOW()) DESC,
+                            DATE(date_updated)>DATE(NOW()) ASC
+                        """.format(id)
+    column_names,query_response = get_query_response(query_request)
+    return get_json_response(column_names,query_response)
+
+
+@app.get("/alcaldias") # by id
+async def get_unidades():
+    query_request = """
+                        SELECT delegation FROM metro_position
+                        GROUP BY
+                            delegation
+                        """
+    column_names,query_response = get_query_response(query_request)
+    return get_json_response(column_names,query_response)
+
+@app.get("/units/alcaldia/{alcaldia}") # by id
+async def get_unidades(alcaldia):
+    query_request = """
+                        SELECT * FROM metro_position
+                        WHERE
+                            delegation = '{}'
+                        ORDER BY
+                            DATE(date_updated)=DATE(NOW()) DESC,
+                            DATE(date_updated)<DATE(NOW()) DESC,
+                            DATE(date_updated)>DATE(NOW()) ASC
+                        """.format(alcaldia)
+    column_names,query_response = get_query_response(query_request)
+    return get_json_response(column_names,query_response)
+
 
 def convert_datetime(datetime):
     return datetime.strftime("%m/%d/%Y %H:%M:%S")
+
+def get_query_response(query):
+    connection,cursor = open_connection()
+    cursor.execute(query)
+    column_names = [desc[0] for desc in cursor.description]
+    query_response = cursor.fetchall()
+    cursor.close()
+    close_connection(connection)
+    return column_names,query_response
 
 def get_json_response(column_names,query_response):
     api_response = '{ "response":['
@@ -73,6 +89,3 @@ def get_json_response(column_names,query_response):
         api_response += json.dumps(response_dic)+','
     api_response = api_response[:-1] + ']}'
     return json.loads(api_response)
-
-
-get_unidades()
